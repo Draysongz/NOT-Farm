5551b26c55b184315a19b3af32e20d26847f0928
draysongz <dharmiegra4@gmail.com>
2024-05-23 11:08:08 +0100

    done

--- src/pages/index.js
+++ src/pages/index.js
@@ -15,7 +15,7 @@ export default function Home() {
     <>
       <Head>
         <title>Not Miner</title>
-        <meta name="description" content="Tonzi Miner" />
+        <meta name="description" content="Not Miner" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
